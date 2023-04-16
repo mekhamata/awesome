cadb4cf77576ab4d761f88811e59b64d742f54f9
Michael Matta <mekha.r99@gmail.com>
2023-04-17 01:39:06 +0300

    adding new user

--- users.js
+++ users.js
@@ -2,6 +2,7 @@ const User = [
   { id: 1, name: "Mikha" },
   { id: 2, name: "Samar" },
   { id: 3, name: "Ghinwa" },
+  { id: 4, name: "Johnny" },
 ];
 
 const bikes = [{ type: "fast", color: "blue" }];
